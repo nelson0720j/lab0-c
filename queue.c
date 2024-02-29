a9361a5a476e587d0a65f68da2bfa9f59ccc48f7
傅孟楷 <ikaros16248@gmail.com>
2024-03-01 01:20:25 +0800

    Feat: implement remove_head/tail

--- queue.c
+++ queue.c
@@ -73,13 +73,31 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+    element_t *entry = list_first_entry(head, element_t, list);
+    if (sp && entry) {
+        strncpy(sp, entry->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    list_del(&entry->list);
+    return entry;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+    element_t *entry = list_last_entry(head, element_t, list);
+    if (sp && entry) {
+        strncpy(sp, entry->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    list_del(&entry->list);
+    return entry;
 }
 
 /* Return number of elements in queue */
