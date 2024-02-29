f21c675eadc14b3e14ca3c297efe37cb1b3b1e1a
傅孟楷 <ikaros16248@gmail.com>
2024-03-01 02:35:40 +0800

    Feat: Implement size

--- queue.c
+++ queue.c
@@ -103,7 +103,15 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head || list_empty(head)) {
+        return 0;
+    }
+    int count = 0;
+    struct list_head *li;
+    list_for_each (li, head) {
+        count++;
+    }
+    return count;
 }
 
 /* Delete the middle node in queue */
