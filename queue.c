a833d48a9fbdbdb1def69bc1033e134e1433d54d
傅孟楷 <ikaros16248@gmail.com>
2024-03-02 19:48:51 +0800

    Feat: Implement ascend and descend

--- queue.c
+++ queue.c
@@ -205,17 +205,50 @@ void q_sort(struct list_head *head, bool descend) {}
  * the right side of it */
 int q_ascend(struct list_head *head)
 {
-    // https://leetcode.com/problems/remove-nodes-from-linked-list/
-    return 0;
+    if (!head || list_empty(head))
+        return 0;
+    struct list_head *tmp = head->next;
+    char *curMin = list_entry(tmp, element_t, list)->value;
+    tmp = tmp->next;
+    while (tmp != head) {
+        element_t *entry = list_entry(tmp, element_t, list);
+        if (strcmp(curMin, entry->value) <= 0) {
+            struct list_head *del = tmp;
+            tmp = tmp->next;
+            list_del(del);
+            q_release_element(entry);
+        } else {
+            curMin = entry->value;
+            tmp = tmp->next;
+        }
+    }
+    return q_size(head);
 }
 
 /* Remove every node which has a node with a strictly greater value anywhere to
  * the right side of it */
 int q_descend(struct list_head *head)
 {
-    // https://leetcode.com/problems/remove-nodes-from-linked-list/
-    return 0;
+    if (!head || list_empty(head))
+        return 0;
+    struct list_head *tmp = head->next;
+    char *curMax = list_entry(tmp, element_t, list)->value;
+    tmp = tmp->next;
+    while (tmp != head) {
+        element_t *entry = list_entry(tmp, element_t, list);
+        if (strcmp(curMax, entry->value) >= 0) {
+            struct list_head *del = tmp;
+            tmp = tmp->next;
+            list_del(del);
+            q_release_element(entry);
+        } else {
+            curMax = entry->value;
+            tmp = tmp->next;
+        }
+    }
+    return q_size(head);
 }
+
 
 /* Merge all the queues into one sorted queue, which is in ascending/descending
  * order */
